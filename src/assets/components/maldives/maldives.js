64ae42fb3ddd88388deada14ff7aea1ca769351f
Niclas Hedegaard Jensen <4251729@RTS-365.dk>
2020-06-04 08:58:43 +0200

    add gitignore

--- src/assets/components/maldives/maldives.js
+++ src/assets/components/maldives/maldives.js
@@ -1,10 +1,10 @@
-const client = contentful.createClient({
+ const client = contentful.createClient({
   space: '3s5j27n9nypm', // contentful space_id
   environment: 'master', // defaults to 'master' if not set
   accessToken: 'yR2gL3HeWPJLJS2umEzQG38R-N1ACaHaWq4_TVFO5F0' // contentful access token
 })
 
-console.log(contentful);
+
 
  const contentful = require('contentful')
 client.getEntry('12vyIdBXEyGTTI6YuaWB57') // entry id
@@ -17,4 +17,4 @@ client.getEntry('12vyIdBXEyGTTI6YuaWB57') // entry id
   document.querySelector(".App__title").innerHTML = entry.fields.title;
   document.querySelector(".App__desc").innerHTML = entry.fields.description;
 })
-.catch(console.error) 
+.catch(console.error)  
